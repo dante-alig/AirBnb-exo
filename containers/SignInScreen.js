64ad7e091ef844cad4f99454063407c8fbfb2d76
dante-alig <samuel.celeste@gmail.com>
2024-08-12 15:26:58 -0400

    V2

--- containers/SignInScreen.js
+++ containers/SignInScreen.js
@@ -1,7 +1,9 @@
 import React from "react";
 import { useNavigation } from "@react-navigation/core";
+import { KeyboardAwareScrollView } from "react-native-keyboard-aware-scroll-view";
+import { useState } from "react";
+import axios from "axios";
 import {
-  Button,
   Text,
   TextInput,
   View,
@@ -11,45 +13,107 @@ import {
   Pressable,
 } from "react-native";
 import logoBnb from "../assets/images/logo-airbnb.png";
+import { ActivityIndicator } from "react-native-paper";
 
 export default function SignInScreen({ setToken }) {
   const navigation = useNavigation();
+  const [username, setUsername] = useState("");
+  const [password, setPassword] = useState("");
+  const [userError, setUserError] = useState("");
+  const [loading, setLoading] = useState(false);
+
+  // ------------FETCH-----------------
+  const fetchData = async () => {
+    try {
+      setLoading(true);
+      const formData = {
+        email: username,
+        password: password,
+      };
+
+      const response = await axios.post(
+        "https://lereacteur-bootcamp-api.herokuapp.com/api/airbnb/user/log_in",
+        formData
+      );
+
+      console.log(response.data);
+      alert("connecté");
+      // const userToken = "secret-token";
+      // setToken(userToken);
+    } catch (error) {
+      console.log(error);
+      setUserError(
+        "votre adress email ou votre mot de passe ne sont pas correct"
+      );
+    }
+    setLoading(false);
+  };
+
   return (
-    <View>
-      <View style={styles.imageBox}>
-        <Image style={styles.imageBnb} source={logoBnb} />
-        <Text style={styles.logoText}>Sign In</Text>
-      </View>
-      <View style={styles.formBox}>
-        <TextInput style={styles.input} placeholder="Username" />
-        <TextInput
-          style={styles.input}
-          placeholder="Password"
-          secureTextEntry={true}
-        />
-        <View style={styles.buttonBox}>
-          <Pressable
-            style={styles.buttonPressable}
-            onPress={async () => {
-              const userToken = "secret-token";
-              setToken(userToken);
-            }}
-          >
-            <Text style={styles.textButton}>sign in</Text>
-          </Pressable>
+    <KeyboardAwareScrollView extraScrollHeight={20}>
+      {/* LOGO */}
+      {loading && <ActivityIndicator style={styles.loading} />}
+      <View>
+        <View style={styles.imageBox}>
+          <Image style={styles.imageBnb} source={logoBnb} />
+          <Text style={styles.logoText}>Sign In</Text>
         </View>
 
-        <TouchableOpacity
-          onPress={() => {
-            navigation.navigate("SignUp");
-          }}
-        >
-          <View style={styles.buttonSubTitleBox}>
-            <Text style={styles.buttonSubTitle}>No account ? Register</Text>
+        {/* FORMULAIRE */}
+        <View style={styles.formBox}>
+          <TextInput
+            style={styles.input}
+            placeholder="Username"
+            onChangeText={(text) => {
+              setUsername(text);
+            }}
+            value={username}
+          />
+          <TextInput
+            style={styles.input}
+            placeholder="Password"
+            secureTextEntry={true}
+            onChangeText={(text) => {
+              setPassword(text);
+            }}
+            value={password}
+          />
+          <View style={styles.errorUserBox}>
+            <Text style={styles.errorUser}>{userError}</Text>
           </View>
-        </TouchableOpacity>
+
+          {/* BOUTON du formulaire */}
+          <View style={styles.buttonBox}>
+            <Pressable
+              style={
+                loading ? styles.buttonPressableFalse : styles.buttonPressable
+              }
+              onPress={async () => {
+                if (username && password) {
+                  fetchData();
+                } else {
+                  setUserError("Please fill all fields");
+                }
+              }}
+              disabled={loading}
+            >
+              <Text style={loading && styles.textButton}>sign In</Text>
+            </Pressable>
+          </View>
+
+          {/* No account ? Register     */}
+          <TouchableOpacity
+            onPress={() => {
+              navigation.navigate("SignUp");
+            }}
+          >
+            <View style={styles.buttonSubTitleBox}>
+              <Text style={styles.buttonSubTitle}>No account ? Register</Text>
+            </View>
+          </TouchableOpacity>
+        </View>
       </View>
-    </View>
+    </KeyboardAwareScrollView>
   );
 }
 
@@ -85,6 +149,16 @@ const styles = StyleSheet.create({
     alignItems: "center",
     justifyContent: "center",
   },
+  buttonPressableFalse: {
+    borderColor: "#C6C6C8",
+    borderWidth: 3,
+    width: 150,
+    height: 50,
+    borderRadius: 20,
+    alignItems: "center",
+    justifyContent: "center",
+  },
+
   buttonBox: {
     alignItems: "center",
     marginTop: 70,
@@ -96,5 +170,21 @@ const styles = StyleSheet.create({
   },
   buttonSubTitleBox: {
     alignItems: "center",
+  },
+  errorUser: {
+    color: "red",
+  },
+  errorUserBox: {
+    alignItems: "center",
+  },
+  loading: {
+    top: 15,
+    margin: "auto",
+    position: "absolute",
+    left: "50%",
+    transform: [{ translateX: -15 }, { translateY: 0 }],
+  },
+  textButton: {
+    color: "#C4C4C6",
   },
 });
--- containers/SignUpScreen.js
+++ containers/SignUpScreen.js
@@ -1,21 +1,217 @@
-import { Button, Text, TextInput, View } from "react-native";
+import { KeyboardAwareScrollView } from "react-native-keyboard-aware-scroll-view";
+import { useState } from "react";
+import { ActivityIndicator } from "react-native-paper";
+import axios from "axios";
+import {
+  Button,
+  Text,
+  TextInput,
+  View,
+  StyleSheet,
+  Pressable,
+  Image,
+  ScrollView,
+} from "react-native";
+import logoBnb from "../assets/images/logo-airbnb.png";
 
 export default function SignUpScreen({ setToken }) {
+  const [email, setEmail] = useState("");
+  const [username, setUsername] = useState("");
+  const [description, setDescription] = useState("");
+  const [password, setPassword] = useState("");
+  const [confirmPassword, setConfirmPassword] = useState("");
+  const [userError, setUserError] = useState("");
+  const [loading, setLoading] = useState(false);
+
+  // ------------FETCH-----------------
+  const fetchData = async () => {
+    console.log(email, username, description, password);
+    try {
+      setLoading(true);
+      const formData = {
+        email: email,
+        username: username,
+        description: description,
+        password: password,
+      };
+
+      const response = await axios.post(
+        "https://lereacteur-bootcamp-api.herokuapp.com/api/airbnb/user/sign_up",
+        formData
+      );
+
+      console.log(response.data);
+      alert("connecté");
+      // const userToken = "secret-token";
+      // setToken(userToken);
+    } catch (error) {
+      console.log(error);
+      setUserError(
+        "votre adress email ou votre mot de passe ne sont pas correct"
+      );
+    }
+    setLoading(false);
+  };
+
   return (
-    <View>
-      <View>
-        <Text>Name: </Text>
-        <TextInput placeholder="Username" />
-        <Text>Password: </Text>
-        <TextInput placeholder="Password" secureTextEntry={true} />
-        <Button
-          title="Sign up"
-          onPress={async () => {
-            const userToken = "secret-token";
-            setToken(userToken);
-          }}
-        />
-      </View>
-    </View>
+    <ScrollView>
+      <KeyboardAwareScrollView extraScrollHeight={20}>
+        {loading && <ActivityIndicator style={styles.loading} />}
+        <View style={styles.imageBox}>
+          <Image style={styles.imageBnb} source={logoBnb} />
+          <Text style={styles.logoText}>Sign Up</Text>
+        </View>
+        <View>
+          <TextInput
+            style={styles.input}
+            placeholder="email"
+            onChangeText={(text) => {
+              setEmail(text);
+            }}
+            value={email}
+          />
+          <TextInput
+            style={styles.input}
+            placeholder="username"
+            onChangeText={(text) => {
+              setUsername(text);
+            }}
+            value={username}
+          />
+          <TextInput
+            style={styles.textArea}
+            multiline
+            numberOfLines={10}
+            placeholder="description"
+            onChangeText={(text) => {
+              setDescription(text);
+            }}
+            value={description}
+          />
+          <TextInput
+            style={styles.input}
+            secureTextEntry={true}
+            placeholder="password"
+            onChangeText={(text) => {
+              setPassword(text);
+            }}
+            value={password}
+          />
+          <TextInput
+            style={styles.input}
+            secureTextEntry={true}
+            placeholder="confirm password"
+            onChangeText={(text) => {
+              setConfirmPassword(text);
+            }}
+            value={confirmPassword}
+          />
+
+          <View style={styles.errorUserBox}>
+            <Text style={styles.errorUser}>{userError}</Text>
+          </View>
+
+          <View style={styles.buttonBox}>
+            <Pressable
+              style={
+                loading ? styles.buttonPressableFalse : styles.buttonPressable
+              }
+              onPress={async () => {
+                if (email && username && password) {
+                  fetchData();
+                } else {
+                  setUserError("Please fill all fields");
+                }
+              }}
+              disabled={loading}
+            >
+              <Text style={loading && styles.textButton}>sign Up</Text>
+            </Pressable>
+          </View>
+          <View style={styles.buttonSubTitleBox}>
+            <Text style={styles.buttonSubTitle}>
+              Already have an account ? Sign in
+            </Text>
+          </View>
+        </View>
+      </KeyboardAwareScrollView>
+    </ScrollView>
   );
 }
+
+const styles = StyleSheet.create({
+  imageBnb: {
+    height: 137,
+    width: 90,
+    resizeMode: "contain",
+  },
+  imageBox: {
+    alignItems: "center",
+    marginTop: 10,
+  },
+  logoText: {
+    fontSize: 22,
+    color: "#717171",
+  },
+  input: {
+    margin: 30,
+    paddingBottom: 10,
+    borderBottomColor: "#FFBAC0",
+    borderBottomWidth: 2,
+  },
+  textArea: {
+    margin: 30,
+    padding: 10,
+    borderColor: "#FFBAC0",
+    borderWidth: 2,
+    height: 120,
+  },
+  buttonPressable: {
+    borderColor: "#F9575C",
+    borderWidth: 3,
+    width: 150,
+    height: 50,
+    borderRadius: 20,
+    alignItems: "center",
+    justifyContent: "center",
+  },
+  buttonPressableFalse: {
+    borderColor: "#C6C6C8",
+    borderWidth: 3,
+    width: 150,
+    height: 50,
+    borderRadius: 20,
+    alignItems: "center",
+    justifyContent: "center",
+  },
+  buttonBox: {
+    alignItems: "center",
+    marginTop: 35,
+  },
+  buttonSubTitle: {
+    marginTop: 20,
+    fontSize: 15,
+    color: "#717171",
+    marginBottom: 70,
+  },
+  buttonSubTitleBox: {
+    alignItems: "center",
+  },
+  loading: {
+    top: 15,
+    margin: "auto",
+    position: "absolute",
+    left: "50%",
+    transform: [{ translateX: -15 }, { translateY: 0 }],
+    zIndex: 10,
+  },
+  textButton: {
+    color: "#C4C4C6",
+  },
+  errorUser: {
+    color: "red",
+  },
+  errorUserBox: {
+    alignItems: "center",
+  },
+});
